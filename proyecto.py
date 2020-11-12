557b7c41a486f5e06bab51a73f6724f650cd2ff5
Huenpe <naranjopehuen@gmail.com>
2020-11-12 18:08:28 -0300

    funciona para 1 solo pais, sin elegir fecha

---
+++ proyecto.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('full_data.csv')
+rd = df.to_dict('list')
+longitud = len(rd['location'])
+
+xdates = []
+ytotalcases = []
+ytotaldeaths = []
+
+print('Deberá ingresar fechas con el formato: año-mes-día (ej: 2020-05-07)')
+welcome = (input('Ingrese el país deseado: ')).lower()
+initialdate = (input('Ingrese fecha de inicio: '))
+finaldate = (input('Ingrese fecha de final: '))
+# timeinterval = in
+for a in range(0,longitud):
+    if (rd['location'][a]).lower() == welcome:
+        xdates.append(rd['date'][a])
+        ytotalcases.append(rd['total_cases'][a])
+        ytotaldeaths.append(rd['total_deaths'][a])
+
+plt.figure(figsize=(12,5))
+plt.xlabel('fechas')
+plt.ylabel('personas en millones')
+plt.yscale('log')
+plt.plot(xdates, ytotalcases, 'g-', label='contagiados')
+plt.plot(xdates, ytotaldeaths, 'r-', label='fallecidos')
+plt.title('coronavirus en '+ welcome)
+plt.xticks(xdates[::30], rotation=30)
+plt.legend()
+plt.show()
+print()
