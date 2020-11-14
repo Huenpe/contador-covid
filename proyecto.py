b29cdb178874fcfbd9cb1b1afeaf040a21f319d2
Huenpe <naranjopehuen@gmail.com>
2020-11-14 13:43:22 -0300

    comentado, validador input cant paises

--- proyecto.py
+++ proyecto.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('full_data.csv')
 rd = df.to_dict('list')
-longitud = len(rd['location'])
-position = 1
-countrytable = []
-for k in range(0,longitud):                            
+longitud = len(rd['location'])  # hice esto para depues de esto hacer las listas generales de un pais
+position = 1     # sirve para asignar posiciones a los graficos
+countrytable = []    #lista con los nombres de todos los paises en el archivo
+for k in range(0,longitud):                            #hace la lista con los nombres de los paises pero en minuscula asi es mas facil hacer que coincida el input.
     countrytable.append((rd['location'][k]).lower())
 
 print('Deberá ingresar fechas con el formato ISO 8601 .Ej: 2020-05-07. Si el pais a ingresar tiene varias palabras tienen que estar separadas por un espacio.')
@@ -15,8 +15,20 @@ initialdate = (input('Ingrese fecha de inicio: '))
 finaldate = (input('Ingrese fecha de final: '))
 
 countries = []
-cntrynumbr = int(input('Ingrese la cantidad de paises a analizar: '))
-for i in range(cntrynumbr):
+
+while True:                    #valida el input de numero de paises a analizar
+    try:
+        inputnumbr = int(input('Ingrese la cantidad de paises a analizar: '))
+        if inputnumbr < 0:
+            raise Exception
+    except Exception:
+        print('Entrada invalida')
+        continue
+    else:
+        break
+
+
+for i in range(inputnumbr):            # ve si el input de pais es valido o no
     welcome = (input('Ingrese el país deseado: ')).lower()
     if welcome in countrytable:
         if welcome not in countries:
@@ -27,42 +39,42 @@ for i in range(cntrynumbr):
         print('Entrada no valida')
 
 for country in countries:
-    dates = []
-    xdates = []
-    indexinitialdate = None
-    indexfinaldate = None
-    totalcases = []
-    totaldeaths = []
-    ytotalcases = []
-    ytotaldeaths = []
-
-    for a in range(0,longitud):                            #linea 39 a 43
-        if (rd['location'][a]).lower() == country:         #hace listas de todos los datos (fechas,
-            dates.append(rd['date'][a])                    #contagaidos y muertos) del pais
+    dates = []                 #aca se guardan todas las fechas de todo el pais en cuestion
+    xdates = []                   #aca se guardan las fechas entre los valores elegidos
+    indexinitialdate = None         #contiene el indice de la fecha inicial elegida
+    indexfinaldate = None             #contiene el indice de la fecha final elegida
+    totalcases = []             #aca se guardan todos los valores de casos totales del pais
+    totaldeaths = []            #aca se guardan todos los valores de muertes totales del pais
+    ytotalcases = []            # contiene los valores de casos totales entre las fechas elegidas
+    ytotaldeaths = []           # contiene los valores de muertes totales entre las fechas elegidas
+ 
+    for a in range(0,longitud):                  #hace listas de todos los datos (fechas,
+        if (rd['location'][a]).lower() == country:    #contagiados y muertos) del pais     
+            dates.append(rd['date'][a])                    
             totalcases.append(rd['total_cases'][a])
             totaldeaths.append(rd['total_deaths'][a])
-    if (initialdate not in dates):              #linea 44 a 46, soluciona el problema si la fecha inicial o final 
+    if (initialdate not in dates):             #soluciona el problema si la fecha inicial o final 
         initialdate = dates[0]              #elegidas no son una fecha que exista para tal pais en el .csv
         finaldate = dates[len(dates)-1]
-    for b in range(0,len(dates)):                          #linea 47 a 51
-            if dates[b] == initialdate:                    #toma la posicion en la lista dates de la
-                indexinitialdate = b                       # fecha inicial y la final elegidas
+    for b in range(0,len(dates)):                          #toma la posicion en la lista dates de la
+            if dates[b] == initialdate:                    # fecha inicial y la final elegidas
+                indexinitialdate = b                       
             elif dates[b] == finaldate:
                 indexfinaldate = b
-    for c in range(0,len(dates)):                            #linea 52 a 56
-        if (indexinitialdate <= c) & (c <= indexfinaldate):  #hace listas de los datos entre las fechas elegidas
+    for c in range(0,len(dates)):                            #hace listas de los datos entre las fechas elegidas
+        if (indexinitialdate <= c) & (c <= indexfinaldate):  
             xdates.append(dates[c])
             ytotalcases.append(totalcases[c])
             ytotaldeaths.append(totaldeaths[c])
 
-    plt.subplot(int((cntrynumbr/2)+(cntrynumbr%2)),2, position)
+    plt.subplot(int((inputnumbr/2)+(inputnumbr%2)),2, position)   #asigna posiciones dependiendo de la cantidad de paises
     position += 1
-    plt.ylabel('personas')
-    plt.yscale('log')
+    plt.ylabel('personas')         
+    plt.yscale('log')  #hace que la escala sea logaritmica
     plt.plot(xdates, ytotalcases, label='contagiados')
     plt.plot(xdates, ytotaldeaths, label='fallecidos')
-    plt.title('coronavirus en '+ country)
-    plt.xticks([xdates[0], xdates[len(xdates)-1]], visible=True, fontsize=5)
+    plt.title('coronavirus en '+ country) #hace que el titulo sea el correcto para el grafico
+    plt.xticks([xdates[0], xdates[len(xdates)-1]], visible=True, fontsize=5) #hace que solo se vea el primer valor y el ultimo valor de la escala en x
     plt.yticks(fontsize=6)
 
 plt.legend()
